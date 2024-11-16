4f5d92cf6d25fdd302a7aca03f20783388ec377f
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-11-16 18:40:04 -0300

    Use 30 days as cookie max age

--- lib/shop_web/plugs/set_console.ex
+++ lib/shop_web/plugs/set_console.ex
@@ -2,7 +2,7 @@ defmodule ShopWeb.Plugs.SetConsole do
   import Plug.Conn
 
   @valid_consoles ["nintendo", "pc", "playstation", "xbox"]
-  @cookie_max_age :timer.hours(1)
+  @thirty_days_in_seconds 2_592_000
 
   def init(default_console), do: default_console
 
@@ -10,7 +10,7 @@ defmodule ShopWeb.Plugs.SetConsole do
       when console in @valid_consoles do
     conn
     |> assign(:console, console)
-    |> put_resp_cookie("console", console, max_age: @cookie_max_age)
+    |> put_resp_cookie("console", console, max_age: @thirty_days_in_seconds)
   end
 
   def call(%Plug.Conn{:cookies => %{"console" => console}} = conn, _default_console)
@@ -22,6 +22,6 @@ defmodule ShopWeb.Plugs.SetConsole do
   def call(%Plug.Conn{} = conn, default_console) do
     conn
     |> assign(:console, default_console)
-    |> put_resp_cookie("console", default_console, max_age: @cookie_max_age)
+    |> put_resp_cookie("console", default_console, max_age: @thirty_days_in_seconds)
   end
 end
